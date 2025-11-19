baed780762c4970e6c913b1f47799d47c3545279
DanyRoland <eldanielitu@gmail.com>
2025-11-19 15:55:24 -0400

    Update MainActivity.kt

--- task-1/app/src/main/java/com/example/taskusercard/MainActivity.kt
+++ task-1/app/src/main/java/com/example/taskusercard/MainActivity.kt
@@ -5,19 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.taskusercard.ui.theme.TaskUserCardTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,15 +50,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+fun onFollowClick() {
+    // Lógica para seguir al usuario
+}
+
 @Composable
-fun UserCard(){
+fun UserCard(nombre: String, fotoUrl: String, onFollowClick: () -> Unit) {
+    var status = "Online" // Estado del usuario
+//    foto =
     Column(modifier = Modifier.padding(16.dp)) {
         Row(){
-            Image() // Imagen de perfil, accedido mediante vínculo
-            Text(text = "John Doe", style = MaterialTheme.typography.h6) // Nombre
-            Text() // Estado
+            Image(
+                painter = painterResource(id = R.drawable.pvz_logo), // Arreglar cuando recuerde cómo se importaban fotos de urls
+                contentDescription = null)
+            Text(text = nombre)
+            Text(text = status) // Estado
         }
-        Button(){
+        Button(onClick = onFollowClick) {
             Text(text = "Follow") // Botón de seguir
         }
     }
