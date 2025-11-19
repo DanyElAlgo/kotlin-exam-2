572b3d0cbd17e63bcf2c895b61f9d55b2e2c5331
DanyRoland <eldanielitu@gmail.com>
2025-11-19 15:46:24 -0400

    UI simple
    enseguida hago el patrón state

--- task-1/app/src/main/java/com/example/taskusercard/MainActivity.kt
+++ task-1/app/src/main/java/com/example/taskusercard/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.taskusercard
 
+import ads_mobile_sdk.h6
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.content.MediaType.Companion.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.taskusercard.ui.theme.TaskUserCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +46,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun UserCard(){
-
+    Column(modifier = Modifier.padding(16.dp)) {
+        Row(){
+            Image() // Imagen de perfil, accedido mediante vínculo
+            Text(text = "John Doe", style = MaterialTheme.typography.h6) // Nombre
+            Text() // Estado
+        }
+        Button(){
+            Text(text = "Follow") // Botón de seguir
+        }
+    }
 }
 
 @Preview(showBackground = true)
