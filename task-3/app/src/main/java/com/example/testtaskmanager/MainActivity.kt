71ff0413113bd85b11e31eed651aae7df6c019fa
DanyRoland <eldanielitu@gmail.com>
2025-11-22 06:33:51 -0400

    initial stuff

--- task-3/app/src/main/java/com/example/testtaskmanager/MainActivity.kt
+++ task-3/app/src/main/java/com/example/testtaskmanager/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import com.example.testtaskmanager.ui.theme.TestTaskManagerTheme
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,6 +31,34 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+data class Task(
+    val id: String = UUID.randomUUID().toString(),
+    val title: String,
+    val isCompleted: Boolean = false
+)
+class TaskViewModel : ViewModel() {
+    private val _tasks = mutableStateListOf<Task>()
+    val tasks: List<Task> get() = _tasks
+
+    fun addTask(title: String) {
+        if (title.isNotBlank()) {
+            _tasks.add(Task(title = title))
+        }
+    }
+
+    fun toggleTaskStatus(taskId: String) {
+        val index = _tasks.indexOfFirst { it.id == taskId }
+        if (index != -1) {
+            val task = _tasks[index]
+            _tasks[index] = task.copy(isCompleted = !task.isCompleted)
+        }
+    }
+
+    fun deleteTask(taskId: String) {
+        _tasks.removeAll { it.id == taskId }
     }
 }
 
