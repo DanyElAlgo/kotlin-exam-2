949fa3e1eebe2c026250fc7835c53fb65175adaf
DanyRoland <eldanielitu@gmail.com>
2025-11-22 06:40:58 -0400

    tarjeta creada
    falta hacer la pantalla

--- task-3/app/src/main/java/com/example/testtaskmanager/MainActivity.kt
+++ task-3/app/src/main/java/com/example/testtaskmanager/MainActivity.kt
@@ -4,16 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.error
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
+import androidx.wear.compose.foundation.weight
 import com.example.testtaskmanager.ui.theme.TestTaskManagerTheme
 import java.util.UUID
 
@@ -24,8 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TestTaskManagerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TaskScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -63,17 +85,46 @@ class TaskViewModel : ViewModel() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskScreen(
+    modifier: Modifier = Modifier,
+    viewModel: TaskViewModel = viewModel()
+) {
+    var newTaskText by remember { mutableStateOf("") }
+//    Desarrollar pantalla
+}
+
+@Composable
+fun TaskItem(
+    task: Task,
+    onCheckedChange: (Boolean) -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = task.title,
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 8.dp),
+                textDecoration = if (task.isCompleted) TextDecoration.LineThrough else null,
+                color = if (task.isCompleted) Color.Gray else Color.Unspecified
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestTaskManagerTheme {
-        Greeting("Android")
+        TaskScreen(modifier = Modifier.padding(16.dp))
     }
 }
